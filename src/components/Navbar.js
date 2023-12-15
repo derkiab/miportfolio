9f88036f40d85675b842ab4f115c6e6aa495f00c
derkiab <110829988+derkiab@users.noreply.github.com>
2023-12-14 22:15:36 -0300

    mui

---
+++ src/components/Navbar.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+
+function Navbar() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Navbar
+        </Typography>
+        {isMobile ? (
+          <>
+            <Button color="inherit" onClick={handleDrawerToggle}>
+              Menu
+            </Button>
+            <Drawer
+              anchor="right"
+              open={drawerOpen}
+              onClose={handleDrawerToggle}
+            >
+              <List>
+                <ListItem>
+                  <Button color="inherit">Option 1</Button>
+                </ListItem>
+                <ListItem>
+                  <Button color="inherit">Option 2</Button>
+                </ListItem>
+                <ListItem>
+                  <Button color="inherit">Option 3</Button>
+                </ListItem>
+              </List>
+            </Drawer>
+          </>
+        ) : (
+          <>
+            <Button color="inherit">Option 1</Button>
+            <Button color="inherit">Option 2</Button>
+            <Button color="inherit">Option 3</Button>
+          </>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Navbar;
+
