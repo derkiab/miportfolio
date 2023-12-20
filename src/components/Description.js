74edad7f78d5580fac3477e35c0c0f1d44246753
derkiab <110829988+derkiab@users.noreply.github.com>
2023-12-20 10:29:52 -0300

    Experiencia timeline added, missing Projects

--- src/components/Description.js
+++ src/components/Description.js
@@ -7,6 +7,9 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import { Paper } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
+import Timeline from './Timeline';
+import { Card, CardContent, CardHeader, Divider, TextField } from '@mui/material';
+
 
 const technologies = [
     { name: 'React', logo: './reactjs-icon.svg' },
@@ -68,7 +71,7 @@ function Technology({ technology }) {
 }
 
 function Description({ navbarHeight }) {
-    
+
     const theme = useTheme();
     const [refNombre, inView1] = useInView({
         triggerOnce: false,
@@ -91,10 +94,18 @@ function Description({ navbarHeight }) {
     const [refAvatar, inView7] = useInView({
         triggerOnce: false,
     });
-
+    const [refExperiencia, inView8] = useInView({
+        triggerOnce: false,
+    });
+    const [refTimeline, inView9] = useInView({
+        triggerOnce: false,
+    });
+    const [refEnd, inView10] = useInView({
+        triggerOnce: false,
+    });
 
     return (
-        <Container maxWidth="xl" sx={{ minHeight: '100%' }}>
+        <Container maxWidth="xl" sx={{ minHeight: '100%', pb: '15vh' }}>
             <Box
                 sx={{
                     position: 'relative',
@@ -160,14 +171,14 @@ function Description({ navbarHeight }) {
                                 opacity: inView1 ? 1 : 0,
                                 transform: inView1 ? 'translateX(0)' : 'translateX(-300px)',
                                 '&:hover': {
-                                    backgroundColor: 'rgba(0,0,0,0.3)', 
-                                    transform: 'scale(1.1)', 
+                                    backgroundColor: 'rgba(0,0,0,0.3)',
+                                    transform: 'scale(1.1)',
                                 },
                             }}
                             href="https://github.com/derkiab"
                             target="_blank"
                             rel="noopener noreferrer"
-                            style={{ color: '#ced4f0', width: '50px', height: '50px' }} 
+                            style={{ color: '#ced4f0', width: '50px', height: '50px' }}
                         >
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
@@ -189,14 +200,14 @@ function Description({ navbarHeight }) {
                                 opacity: inView1 ? 1 : 0,
                                 transform: inView1 ? 'translateX(0)' : 'translateX(-300px)',
                                 '&:hover': {
-                                    backgroundColor: 'rgba(0,0,0,0.3)', 
-                                    transform: 'scale(1.1)', 
+                                    backgroundColor: 'rgba(0,0,0,0.3)',
+                                    transform: 'scale(1.1)',
                                 },
                             }}
                             href="https://www.linkedin.com/in/derquisanhueza/"
                             target="_blank"
                             rel="noopener noreferrer"
-                            style={{ color: '#ced4f0', width: '50px', height: '50px', }} 
+                            style={{ color: '#ced4f0', width: '50px', height: '50px', }}
                         >
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6" aria-hidden="true">
                                 <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
@@ -358,6 +369,59 @@ function Description({ navbarHeight }) {
                 <br />
                 <br />
                 <br />
+                <Typography
+                    ref={refExperiencia}
+                    variant="h2"
+                    sx={{
+                        fontWeight: 'normal',
+                        color: '#ced4f0',
+                        fontFamily: 'Roboto, sans-serif',
+                        transition: 'opacity 0.8s ease-out, transform 0.8s ease-out',
+                        opacity: inView8 ? 1 : 0,
+                        transform: inView8 ? 'translateX(0)' : 'translateX(-200px)',
+                        textShadow: inView8 ? '0px 4px 4px rgba(0, 0, 0, 0.5)' : 'none',
+                        pt: '5vh',
+                        mt: '10vh',
+                        mb: '12vh',
+                        [theme.breakpoints.down('sm')]: {
+                            fontSize: '1.6rem',
+                            mt: '15vh'
+                        },
+                    }}
+                >
+                    Experiencia
+                </Typography>
+                <div ref={refTimeline} style={{
+                    opacity: inView9 ? 1 : 0,
+                    transition: 'opacity 2.5s ease-in, transform 1.8s ease-out',
+                    transform: inView9 ? 'translateX(0)' : 'translateX(-300px)',
+                    marginBottom: '10vh',
+                }}>
+                    <Timeline />
+                </div>
+
+                <Typography
+                    variant="h10"
+                    ref={refEnd}
+                    sx={{
+                        fontWeight: 'light',
+                        fontStyle: 'italic',
+                        fontSize: '0.8rem',
+                        transition: 'opacity 0.8s ease-out, transform 1.8s ease-out',
+                        color: '#ced4f0',
+                        opacity: inView10 ? 1 : 0,
+                        fontFamily: 'Roboto, sans-serif',
+                        mt: '10vh',
+                        textShadow: inView10 ? '0px 4px 4px rgba(0, 0, 0, 0.5)' : 'none',
+                        transform: inView10 ? 'translateX(0)' : 'translateX(-300px)',
+                        [theme.breakpoints.down('sm')]: {
+                            fontSize: '0.7rem',
+                        },
+                    }}
+                >
+                    Creado por Derqui Sanhueza Balboa, Todos los derechos reservados © 2023
+                    
+                </Typography>
             </Box>
         </Container>
     );
---
+++ src/components/Timeline.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Chrono } from 'react-chrono';
+
+const items = [
+  {
+    title: "Oct 2023",
+    cardTitle: "CEO de TakeCare",
+    cardBgColor: "black",
+    cardSubtitle: "Proyecto de emprendimiento innovador IDEA e Innova",
+    cardDetailedText: "Adjudicación de fondo concursable de la UCSC",
+
+  },
+  {
+    title: "Abr 2023",
+    cardTitle: "Desarrollador de Software",
+    cardSubtitle: "Dirección de Investigación UCSC",
+    cardDetailedText: "Desarrollo de aplicación web con Laravel 10",
+
+  },
+  {
+    title: "Ene 2023",
+    cardTitle: "Ingeniero de datos",
+    cardSubtitle: "Soc. Pesquera Landes S.A.",
+    cardDetailedText: "Tratamiento de datos en Data Factory en la nube de Azure, Microsoft Dataverse, Data Lake Gen v2 y SQL Server on-premises. Implementación de pipelines. Generación de reportes en PowerBi. Automatización de procesos con Power Automate y Inteligencia Artificial. ",
+
+  }
+];
+
+const VerticalTimeline = () => {
+  return (
+    
+      <Chrono
+        items={items}
+        mode="VERTICAL"
+        useReadMore={false}
+        theme={{
+          primary: "#79979f",
+          secondary: "rgba(15,23,42,0.8)",
+          cardBgColor: "rgba(170,176,203,0.1)",
+          cardForeColor: "white",
+          titleColor: "#ced4f0",
+          cardSubtitleColor: "#ced4f0",
+          cardTitleColor: "#ced4f0",
+          titleColorActive: "#eee1c8",
+          detailsColor: "#eee1c8",
+          cardDetailsColor: "#eee1c8",
+        }}
+        scrollable={{ scrollbar: true }}
+        cardHeight={100}
+        cardWidth={650}
+        timelinePointDimension={14}
+        hideControls={true}
+        fontSizes={{
+          cardSubtitle: '1.2rem',
+          cardText: '1rem',
+          cardTitle: '1.3rem',
+          title: '1rem',
+          cardDetailedText: '1.2rem',
+        }}
+      />
+    
+  );
+};
+
+export default VerticalTimeline;
